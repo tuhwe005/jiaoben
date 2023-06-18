4eeca341f50f3ed54d2c1827a45db568da75b32e
tuhwe005 <96981681+tuhwe005@users.noreply.github.com>
2023-06-19 01:48:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,10 @@ wget -q https://raw.githubusercontent.com/tuhwe005/jiaoben/main/iptest
 wget -q https://raw.githubusercontent.com/tuhwe005/jiaoben/main/locations.json
 
 # Set execute permission
-chmod +x cccc.sh
+chmod +777 asscan.sh
+chmod +777 masscan
+chmod +777 iptest
+chmod +777 locations.json
 
-# Run cccc.sh script
+# Run asscan.sh script
 ./asscan.sh
